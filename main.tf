124d5277bf224aa72cc202d3a7875d5b6d50063e
Darin Pope <darin@planetpope.com>
2021-08-06 12:13:40 -0400

    add bad example with commented out good
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+locals {
+  password = "p4ssw0rd"
+}
+
+resource "evil_corp" "bad_example" {
+	root_password = local.password
+}
+
+# variable "password" {
+#   description = "The root password for our VM"
+#   type        = string
+# }
+
+# resource "evil_corp" "good_example" {
+# 	root_password = var.password
+# }
+
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
