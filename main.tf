a9a68ed45c96e935f064bdd49db6bc5c1b360c82
Darin Pope <darin@planetpope.com>
2021-09-27 16:29:31 -0400

    update
    Signed-off-by: Darin Pope <darin@planetpope.com>

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-# locals {
-#   password = "p4ssw0rd"
-# }
-
-# resource "evil_corp" "bad_example" {
-# 	root_password = local.password
-# }
-
-variable "password" {
-  description = "The root password for our VM"
-  type        = string
+locals {
+  password = "p4ssw0rd"
 }
 
-resource "evil_corp" "good_example" {
-	root_password = var.password
+resource "evil_corp" "bad_example" {
+	root_password = local.password
 }
+
+# variable "password" {
+#   description = "The root password for our VM"
+#   type        = string
+# }
+
+# resource "evil_corp" "good_example" {
+# 	root_password = var.password
+# }
 
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
